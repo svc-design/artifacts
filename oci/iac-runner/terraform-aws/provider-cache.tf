6ffc8aa2579e8effd400f89fd5c70552483f2949
shenlan <manbuzhe2009@qq.com>
2023-06-21 10:14:42 +0800

    Update provider-cache.tf
    Signed-off-by: shenlan <manbuzhe2009@qq.com>

--- oci/iac-runner/terraform-aws/provider-cache.tf
+++ oci/iac-runner/terraform-aws/provider-cache.tf
@@ -1,8 +1,18 @@
-
-provider "aws" {} 
-
+provider "aws" {}
 provider "tls" {}
-
 provider "null" {}
-
 provider "template" {}
+
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+    }
+  }
+}
