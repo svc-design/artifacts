054533f574ec45668d0c0c23a626e8887cdeeba8
Haitao Pan <manbuzhe2009@qq.com>
2024-03-05 23:42:15 +0800

    add setup-kaniko-auth.sh

---
+++ oci/base/alpine-image-builder/setup-kaniko-auth.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REGISTRY_ADDR=$1
+REGISTRY_REPO_USER=$2
+REGISTRY_REPO_PASSWORD=$3
+DEBUG=${4:-false}
+
+check_non_empty() {
+    if [ -z "$1" ]; then
+        echo "Error: $2 must be provided."
+        exit 1
+    fi
+}
+
+check_non_empty "$REGISTRY_ADDR" "Registry address"
+check_non_empty "$REGISTRY_REPO_USER" "Registry username"
+check_non_empty "$REGISTRY_REPO_PASSWORD" "Registry password"
+
+mkdir -p /kaniko/.docker
+AUTH=$(echo -n "${REGISTRY_REPO_USER}:${REGISTRY_REPO_PASSWORD}" | base64)
+echo "{\"auths\":{\"https://${REGISTRY_ADDR}/v1/\":{\"auth\":\"${AUTH}\"}}}" > /kaniko/.docker/config.json
+
+if [ "$DEBUG" = "true" ]; then
+    cat /kaniko/.docker/config.json
+fi
