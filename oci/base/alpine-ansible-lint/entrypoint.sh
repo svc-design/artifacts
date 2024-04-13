5aa0bf2fc682e70d2dbab79726b4a7d7e8cbdba0
Haitao Pan <manbuzhe2009@qq.com>
2024-04-13 18:54:18 +0800

    alpine-ansible-lint: entrypoint.sh update

--- oci/base/alpine-ansible-lint/entrypoint.sh
+++ oci/base/alpine-ansible-lint/entrypoint.sh
@@ -3,15 +3,4 @@
 # https://issues.jenkins.io/browse/JENKINS-51307?focusedCommentId=341121&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel
 
 set -e
-
-echo "Starting entrypoint script..."
-
-# Check if there was a command passed
-if [ "$1" ]; then
-    echo "Executing passed command: $@"
-    exec "$@"
-else
-    echo "No command provided, running default application..."
-    # 在这里指定一个默认命令，如果通常没有特定的命令需要执行
-    exec /bin/bash
-fi
+exec "$@"
