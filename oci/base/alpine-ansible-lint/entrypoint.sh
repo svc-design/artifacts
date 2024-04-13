d814cb3c66152df7ab7d24185dcbdd43487a9c26
Haitao Pan <manbuzhe2009@qq.com>
2024-04-13 18:04:23 +0800

    alpine-ansible-lint: add jenkins issues url

--- oci/base/alpine-ansible-lint/entrypoint.sh
+++ oci/base/alpine-ansible-lint/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # check if there was a command passed
 # required by Jenkins Docker plugin: https://github.com/docker-library/official-images#consistency
+# https://issues.jenkins.io/browse/JENKINS-51307?focusedCommentId=341121&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel
 if [ "$1" ]; then
     # execute it
     exec "$@"
