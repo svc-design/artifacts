e561cd35687dbf404afcaa36fdb172e2eecb2845
Haitao Pan <manbuzhe2009@qq.com>
2024-04-13 19:38:04 +0800

    alpine-ansible-lint: update

--- oci/base/alpine-ansible-lint/entrypoint.sh
+++ oci/base/alpine-ansible-lint/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # required by Jenkins Docker plugin: https://github.com/docker-library/official-images#consistency
-# https://issues.jenkins.io/browse/JENKINS-51307?focusedCommentId=341121&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel
 
 set -x
 exec "$@"
